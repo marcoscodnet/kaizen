f0b34f6b4c293ab6b0fcc0d07998c5c734c6881b
mpinia76 <marcos.pinero1976@gmail.com>
2022-11-17 19:10:01 -0300

    varios

--- includes/conf.php
+++ includes/conf.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_CLASS', 'MySQL');
-define('DB_HOST', '163.10.35.37');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'secyt');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'kaizen');
 
 //$row_per_page = 10;
